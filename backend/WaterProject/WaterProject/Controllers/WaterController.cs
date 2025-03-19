77d7b146cc8df14c7fa25cdb784ead4e28f13b15
cooperburden <cooperburden@gmail.com>
2025-03-19 17:41:03 -0600

    initla commit

---
+++ backend/WaterProject/WaterProject/Controllers/WaterController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.Data;
+
+namespace WaterProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _waterContext;
+        
+        public WaterController(WaterDbContext temp)
+        {
+            _waterContext = temp;
+        }
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> Get()
+        {
+            var something = _waterContext.Projects.ToList();
+            return something;
+        }
+
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionProject()
+        {
+            var something = _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+            return something;
+        }
+        
+        
+        
+
+
+    }
+}
---
+++ frontend/src/types/Project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    projectID: number;
+    projectName: string;
+    projectType: string;
+    projectRegionalProgram: string;
+    projectImpact: string;
+    projectPhase: string;
+    projectFunctionalityStatus: string;
+}
